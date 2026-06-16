38abf328ae79b8104ff8652003596be48c2de3ee
RicardoChaves26 <richavesca@est.utn.ac.cr>
2026-06-16 09:50:36 -0600

    Se crearon variables de entorno
    Se crearon variables de entorno y se utilizaron dentro del app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,21 @@
+import dotenv from "dotenv"; //Si la ruta del .env en otro .env, "dotenv/config(.env.development)"
 import express from "express";
+
+dotenv.config();
+
+const NAME=process.env.SERVER_NAME;
+const VERSION=process.env.SERVER_VERSION;
+const DESCRIPTION=process.env.SERVER_DESCRIPTION;
+const PORT=process.env.SERVER_PORT;
 
 const app = express();
 
 app.get("/", (req, res) => {
     res.send(
-        "<h1>Servidor funcionando</h1>"
+        `<h1>${NAME}</h1> <p>${VERSION}</p><p>${DESCRIPTION}</p>`
     )
 });
 
-app.listen(4000, () => {
-    console.log("Servidor funcionando");
+app.listen(PORT, () => {
+    console.log(`$${process.env.NAME} ejecutandose en http://localhost:${PORT}`);
 });
