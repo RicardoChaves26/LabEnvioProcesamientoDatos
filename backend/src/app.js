a64f2d28a436dc7fd910248d8138d7e748c05167
RicardoChaves26 <richavesca@est.utn.ac.cr>
2026-06-16 10:10:35 -0600

    Utilizacion cors
    Se implemento cors para poder usar el backend, exponer el api , y hacer la utilizacion de json para mostrar en la web

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"; //Si la ruta del .env en otro .env, "dotenv/config(.env.development)"
 import express from "express";
+import cors from "cors";
 
 dotenv.config();
 
@@ -10,10 +11,16 @@ const PORT=process.env.SERVER_PORT;
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    res.send(
-        `<h1>${NAME}</h1> <p>${VERSION}</p><p>${DESCRIPTION}</p>`
-    )
+    res.json({
+        name: NAME,
+        version: VERSION, 
+        description: DESCRIPTION,
+        puerto: PORT
+    })
 });
 
 app.listen(PORT, () => {
