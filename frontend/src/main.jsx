411b422803401515041c5b7accd6ffcf6759d743
Ricardo Chaves Campos <richavesca@est.utn.ac.cr>
2026-06-22 22:19:12 -0600

    Imports dentro de main.
    Dentro de main se realiza el import de las clases necesarias de bootstrap para la utilizacion del codigo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import "bootstrap/dist/css/bootstrap.min.css"; //npm instal bootstrap
+import "bootstrap-icons/font/bootstrap-icons.css"; //npm instal bootstrap-icons
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
